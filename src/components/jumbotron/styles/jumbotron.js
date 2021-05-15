14663929b705475912dd14efacadbca26a4e2e53
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-16 00:10:16 +0530

    Restructuring files

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,15 +1,19 @@
 import styled from "styled-components/macro";
+
 export const Inner = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  max-width: 1100px;
   flex-direction: ${({ direction }) => direction};
+  max-width: 1100px;
+  margin: auto;
+  width: 100%;
 
   @media (max-width: 1000px) {
     flex-direction: column;
   }
 `;
+
 export const Pane = styled.div`
   width: 50%;
 
@@ -19,19 +23,17 @@ export const Pane = styled.div`
     text-align: center;
   }
 `;
-export const Container = styled.div``;
+
 export const Title = styled.h1`
   font-size: 50px;
   line-height: 1.1;
   margin-bottom: 8px;
+
   @media (max-width: 600px) {
     font-size: 35px;
   }
 `;
-export const Image = styled.img`
-max-width:100%
-height:auto;
-`;
+
 export const SubTitle = styled.h2`
   font-size: 26px;
   font-weight: normal;
@@ -41,10 +43,24 @@ export const SubTitle = styled.h2`
     font-size: 18px;
   }
 `;
+
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
 export const Item = styled.div`
   display: flex;
   border-bottom: 8px solid #222;
   padding: 50px 5%;
   color: white;
   overflow: hidden;
+`;
+
+export const Container = styled.div`
+  @media (max-width: 1000px) {
+    ${Item}:last-of-type h2 {
+      margin-bottom: 50px;
+    }
+  }
 `;
