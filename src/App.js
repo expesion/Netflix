9388b40e4575d7f703b319b63223bbbe60dfb5c9
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-16 11:49:05 +0530

    Setting up router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import AccordianContainer from "./containers/accordian";
 import FooterContainer from "./containers/footer";
 import JumbotronContainer from "./containers/jumbotron";
+import * as ROUTES from "./constants/routes";
 function App() {
   return (
-    <>
+    <Router>
+      <Route exact path={ROUTES.HOME} />
       <JumbotronContainer />
       <AccordianContainer />
       <FooterContainer />
-    </>
+    </Router>
   );
 }
 
