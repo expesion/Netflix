6fdc0f7c1f6850180d5312dda295e3ecbb989fb6
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-17 18:55:35 +0530

    Film/series data extracting from firebase

---
+++ src/hooks/useContent.js
@@ -0,0 +1,21 @@
+import { useEffect, useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+export default function useContent(target) {
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => {
+        const content = snapshot.docs.map((contentObj) => {
+          return { ...contentObj.data(), docId: contentObj.id };
+        });
+        setContent(content);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return { [target]: content };
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { useContent } from "../hooks";
 function Browse() {
+  const { series } = useContent("series");
+  console.log(series);
   return <div>Browse</div>;
 }
 
