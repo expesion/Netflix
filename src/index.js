8584c583d37154929976818309b5f2176849cefe
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-17 09:25:13 +0530

    Firebase setup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 import App from "./App";
-import { GlobalStyles } from "./globalStyles"; /* 
-import { firebase } from "./lib/firebase.prod"; */
+import { GlobalStyles } from "./globalStyles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 ReactDOM.render(
   <React.StrictMode>
-    <>
+    <FirebaseContext.Provider value={{ firebase }}>
       <GlobalStyles />
       <App />
-    </>
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
