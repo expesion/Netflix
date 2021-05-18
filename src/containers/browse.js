17154765f8b6750c331af913355e70e54676ae24
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-18 10:59:20 +0530

    BrowseContainer

---
+++ src/containers/browse.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BrowseContainer() {
+  return <div>BrowseContainer</div>;
+}
+
+export default BrowseContainer;
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useContent } from "../hooks";
 import selectionFilter from "../utils/selection-filter";
+import BrowseContainer from "../containers/browse";
 function Browse() {
   const { series } = useContent("series");
   const { films } = useContent("films");
   const slides = selectionFilter({ series, films });
   console.log(slides);
-  return <div>Browse</div>;
+  return <BrowseContainer />;
 }
 
 export default Browse;
