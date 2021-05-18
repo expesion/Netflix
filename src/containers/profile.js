8253c08fada2b37b6a3b1d6bf26c237c129e624f
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-18 11:13:47 +0530

    Profile selection page

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -1,12 +1,23 @@
 import React from "react";
 import * as ROUTES from "../constants/routes";
 import { Header } from "../components";
+import { Profile } from "../components/header/style/header";
+import { Profiles } from "../components";
 function SelectProfileContainer({ user, setProfile }) {
   return (
     <Header bg={false}>
       <Header.Frame>
         <Header.Logo to={ROUTES.HOME} src={"images/logo.svg"} alt="netflix" />
       </Header.Frame>
+      <Profile>
+        <Profiles.Title>Who's Watching?</Profiles.Title>
+        <Profile.List>
+          <Profile.User>
+            <Profile.Picture src={user.photoURL} />
+            <Profile.Name>{user.displayName}</Profile.Name>
+          </Profile.User>
+        </Profile.List>
+      </Profile>
     </Header>
   );
 }
